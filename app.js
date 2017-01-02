7cefaa413f2b05c8b314d008778c190c857078ce
Evan Suo <evan.universe.suo@gmail.com>
2017-01-03 10:01:51 +1100

    enable es6 syntax

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/**
+ * Copyright 2017 eggeggTech, Inc.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use
+ * of this software is subject to the Facebook Developer Principles and
+ * Policies [http://developers.facebook.com/policy/]. This copyright notice
+ * shall be included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE
+ */
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const ParseServer = require('parse-server').ParseServer;
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+//setting
+const SERVER_PORT = process.env.PORT || 1337;
+const SERVER_HOST = process.env.HOST || 'localhost';
+const APP_ID = process.env.APP_ID || 'resolver-app-2017';
+const MASTER_KEY = process.env.MASTER_KEY || '3xgjpsvx75gimoin48fegy3l8774xd760dgvbhy8';
+const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://resolveruser:developer@ds037262.mlab.com:37262/resolver';
+const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
 
-var ParseServer = require('parse-server').ParseServer;
+//express
+const app = express();
+
 
 //Parse Server
-var api = new ParseServer({
-    databaseURI: 'mongodb://resolveruser:developer@ds037262.mlab.com:37262/resolver', // Connection string for your MongoDB database
-    appId: 'resolver-app-2017',
-    masterKey: 'developer', // Keep this key secret!
-    serverURL: 'http://localhost:1337/parse' // Don't forget to change to https if needed
+const api = new ParseServer({
+    databaseURI: DATABASE_URI, // Connection string for your MongoDB database
+    appId: APP_ID,
+    masterKey: MASTER_KEY, // Keep this key secret!
+    serverURL: `http://localhost:${SERVER_PORT}/parse` // Don't forget to change to https if needed
 });
 
 // Serve the Parse API on the /parse URL prefix
 app.use('/parse', api);
 
-app.listen(1337, function() {
-    console.log('parse-server-example running on port 1337.');
+app.listen(SERVER_PORT, function() {
+    console.log(`Parse server is running on port ${SERVER_HOST}` );
 });
 
 // view engine setup
@@ -44,7 +74,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
